b3ce05bf8a8fe86d46de081adf99186ebf2e3fea
LEI-111342 <pfmca1@iscte-iul.pt>
2025-12-03 22:47:12 +0000

    undo do MainPage e do MainPageTest

--- src/test/java/com/example/selenium1/MainPage.java
+++ src/test/java/com/example/selenium1/MainPage.java
@@ -1,27 +1,14 @@
 package com.example.selenium1;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
+import com.codeborne.selenide.SelenideElement;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 // page_url = https://www.jetbrains.com/
 public class MainPage {
-    @FindBy(xpath = "//*[@data-test-marker='Developer Tools']")
-    public WebElement seeDeveloperToolsButton;
-
-    @FindBy(xpath = "//*[@data-test='suggestion-link']")
-    public WebElement findYourToolsButton;
-
-    @FindBy(xpath = "//div[@data-test='main-menu-item' and @data-test-marker='Developer Tools']")
-    public WebElement toolsMenu;
-
-    @FindBy(css = "[data-test='site-header-search-action']")
-    public WebElement searchButton;
-
-    @FindBy(css = "button.ch2-allow-all-btn")
-    public WebElement acceptCookiesButton;
-
-    public MainPage(WebDriver driver) {
-        PageFactory.initElements(driver,this);}
+    public SelenideElement seeDeveloperToolsButton = $x("//*[@data-test-marker='Developer Tools']");
+    public SelenideElement findYourToolsButton = $x("//*[@data-test='suggestion-action']");
+    public SelenideElement toolsMenu = $x("//div[@data-test='main-menu-item' and @data-test-marker = 'Team Tools']");
+    public SelenideElement searchButton = $("[data-test='site-header-search-action']");
 }
